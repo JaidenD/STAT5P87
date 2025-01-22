f2297b92e92b882d18a53096cb0fc1841af04744
JaidenD <jaidend1273@gmail.com>
2025-01-21 19:39:40 -0500

    finished question 1

--- Assignment1/Assignment1-1-Dahlke.R
+++ Assignment1/Assignment1-1-Dahlke.R
@@ -1,4 +1,4 @@
-data = read.csv("~/Assignment1/a1-q1-data.csv")
+data = read.csv("~/STAT5P87/Assignment1/a1-q1-data.csv")
 
 # Rename variable 'x' to 'x1'
 data$variable[data$variable == 'x'] <- 'x1'
@@ -10,29 +10,21 @@ data = data[-c(which(is_two==TRUE)),]
 # Add rows to the data frame for a new observation 4 (x1 = 3, y = 2)
 new_data = data.frame(c(4,4), c('x1','y'), c(3,2))
 names(new_data) = c('observation', 'variable', 'value')
-new_data
 
 data = rbind(data,new_data)
-data
+
+# Swap rows so variable order is y,x1,y,x1,...
+data[c(5, 6), ] = data[c(6, 5), ]
 
 # Add rows to the data frame for a new variable x2 
 #(x2(observation = 1) = 3, x2(observation = 3) = 1, x2(observation = 4) = 5)
-is_observation_two = (data$observation == 2)
-is_observation_three = (data$observation == 3)
-is_observation_four = (data$observation == 4)
+newrow1 = data.frame(observation = 2, variable = "x2", value = "3")
+newrow2 = data.frame(observation = 3, variable = "x2", value = "1")
+newrow3 = data.frame(observation = 4, variable = "x2", value = "5")
 
-x_2 = 3*is_observation_two + 1*is_observation_three + 5*is_observation_four
-# I'm not sure if this is correct.
-data$x_2 = x_2
-data
+data = rbind(data[1:2,], newrow1, data[3:4,], newrow2, data[5:6,], newrow3)
 
-# Create  a  new  column  named  ‘value-squared’  containing  the  squaredy,x1  andx2values for each observation
-
-
-
-
-
-
-
+`value-squared` = as.numeric(data$value)^2
+data = cbind(data,`value-squared`)
 
 
