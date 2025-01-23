8bf56fef743a471a4b3840ea2ab202fe7d75aeec
JaidenD <jaidend1273@gmail.com>
2025-01-23 16:18:59 -0500

    fixed question 3

--- Assignment1/Assignment1-3-Dahlke.R
+++ Assignment1/Assignment1-3-Dahlke.R
@@ -4,36 +4,24 @@ test = iris[(seq(nrow(iris))%%2==0),]
 
 # Ridge Regression  Input: x1 = Sepal.Width, x2 = Petal.Length, x3 = Petal.Width
 #                   Output: y = Sepal.Length
-# Compute norm
-norm_vec <- function(x) sqrt(sum(x^2))
-
-# Rescale
-rescale <- function(x) (x-mean(x))/norm_vec(x-mean(x))
 
 inputs = 3
 
-# Rescale inputs
-X_train = apply(train[, c("Sepal.Width", "Petal.Length", "Petal.Width")], 2, rescale)
-X_test = apply(test[, c("Sepal.Width", "Petal.Length", "Petal.Width")], 2, rescale)
+train_features <- train[, c("Sepal.Width", "Petal.Length", "Petal.Width")]
+test_features <- test[, c("Sepal.Width", "Petal.Length", "Petal.Width")]
 
+# Compute training statistics
+xBar <- apply(train_features, 2, mean)
+s <- apply(train_features, 2, sd)
+
+# Scale training and test data using training stats
+X_train <- scale(train_features, center = xBar, scale = s)
+X_test <- scale(test_features, center = xBar, scale = s)  # Use training stats!
 y_train = train$Sepal.Length
 y_test = test$Sepal.Length
 
-# Initial guess
-lambda = 1
-
-# Compute model coefficents
-b0_hat = mean(y_train)
-b_hat = solve(t(X_train) %*% X_train + lambda * inputs) %*% t(X_train) %*% (y_train - b0_hat)
-
-# Make predictions
-y_pred = b0_hat + X_test%*%b_hat
-
-# Estimate mean-squared error
-mean((y_test - y_pred)^2)
-
 # Optimize lambda
-lambda_values = 10^seq(-2, 2, length = 100)
+lambda_values = 10^seq(-2, 0.5, length = 100)
 lambda_amount = length(lambda_values)
 mse = matrix(NA, nrow = lambda_amount)
 
@@ -41,7 +29,7 @@ for(i in 1:lambda_amount){
   lambda = lambda_values[i]
   
   b0_hat = mean(y_train)
-  b_hat = solve(t(X_train) %*% X_train + lambda * inputs) %*% t(X_train) %*% (y_train - b0_hat)
+  b_hat = solve(t(X_train) %*% X_train + lambda * diag(inputs)) %*% t(X_train) %*% (y_train - b0_hat)
   
   # Make predictions on testing data
   y_pred = b0_hat + X_test %*% b_hat
@@ -49,7 +37,8 @@ for(i in 1:lambda_amount){
   # Estimate mean-squared error
   mse[i] = mean((y_test - y_pred)^2)
 }
-
-mse
-plot(lambda_values, mse, bty = 'n', 
+plot(lambda_values, mse, bty = 'n',
      lwd = 2, cex = 1.2)
+
+lambda_values[which(mse == min(mse))]
+
