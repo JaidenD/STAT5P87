121849b1ab606d3a7dbb6e339347952ffd9ee1c2
JaidenD <jaidend1273@gmail.com>
2025-02-26 12:52:18 -0500

    rdjg

---
+++ Test2/Test2Sheet.R
@@ -0,0 +1,25 @@
+#spline methods
+
+# cubic spline
+
+# natural spline
+
+# smoothing spline
+
+# _______________________________________________
+
+# kernel methods
+
+# kernel regression / classification
+
+# local linear regression
+
+# Cross-validation
+
+# K-fold
+# Stratified
+# (Assignment function)
+
+
+
+
